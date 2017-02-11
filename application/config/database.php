fcfb742d833374b40b6f71b90b65142a27d0d742
Ovidiu <ovidiupescar@gmail.com>
2017-02-11 22:27:05 +0000

    configuration for master-spc.premiumstickers.co

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'codeigniter_masterspc',
+	'hostname' => 'master-spc.premiumstickers.co',
+	'username' => 'decorsfu_ahile07',
+	'password' => 'braveheart07',
+	'database' => 'decorsfu_codeigniter',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
