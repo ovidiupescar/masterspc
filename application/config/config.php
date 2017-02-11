edfa9ebdbd245815c08759115135696de46efe6f
Ovidiu <ovidiupescar@gmail.com>
2017-02-11 23:31:48 +0000

    configuration for master-spc.premiumstickers.co

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://master-spc.premiumstickers.co/'; 'baubau'
+$config['base_url'] = 'http://master-spc.premiumstickers.co/';
 
 /*
 |--------------------------------------------------------------------------
