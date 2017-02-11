a341ece1ab5fac10abb4cfb9933c2618019309e3
Ahile07 <corneaflorinandrei@gmail.com>
2017-02-11 21:32:58 +0000

    new form layout

--- application/views/masterspc/newPartNumber.php
+++ application/views/masterspc/newPartNumber.php
@@ -5,37 +5,39 @@
     <br /><br />
     <div class="well"><h1 style="font-size:15pt">Add new part number and critical dimensions!</h1></div>
 
-<form action='admin/createPartNo' method='post'>
+<?php
+echo form_open('admin/createPartNo');
+?>
   <div class=‘form-group'>
     <label for=‘partNo’>Part number:</label>
-    <input type=‘text’ class='form-control' id='partNo'>
+    <input type=‘text’ class='form-control' name='partNo'>
 <br />
     <label for=‘customer’>Customer:</label>
-    <input type=‘text’ class='form-control' id=‘customer’>
+    <input type=‘text’ class='form-control' name='customer'>
 <br />
     <label for=‘material’>Material:</label>
-    <input type='text' class='form-control' id=‘material’>
+    <input type='text' class='form-control' name='material'>
 <br />
     <label for=‘thickness’>Thickness (mm):</label>
-    <input type='text' class='form-control' id='thickness'>
+    <input type='text' class='form-control' name='thickness'>
 <br />
     <label for=‘dim1’>CTQ 1 (mm):</label>
-    <input type='text' class='form-control' id=‘dim1’>
+    <input type='text' class='form-control' name='dim1'>
 <br />
     <label for=‘tol_plus1’>+ tolerance (mm):</label>
-    <input type='text' class='form-control' id='tol_plus1'>
+    <input type='text' class='form-control' name='tol_plus1'>
 <br />
     <label for=‘tol_minus1’>- tolerance (mm):</label>
-    <input type='text' class='form-control' id='tol_minus1'>
+    <input type='text' class='form-control' name='tol_minus1'>
 <br />
     <label for=‘dim2’>CTQ 2 (mm):</label>
-    <input type='text' class='form-control' id=‘dim2’>
+    <input type='text' class='form-control' name='dim2'>
 <br />
     <label for=‘tol_plus2’>+ tolerance (mm):</label>
-    <input type='text' class='form-control' id='tol_plus2'>
+    <input type='text' class='form-control' name='tol_plus2'>
 <br />
     <label for=‘tol_minus2’>- tolerance (mm):</label>
-    <input type='text' class='form-control' id='tol_minus2'>
+    <input type='text' class='form-control' name='tol_minus2'>
   </div>
 <br />
 
